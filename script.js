2dcec3344a7056d2d2f764c10feafa0ce6f1f436
bugflux <bugflux@2fa9e8c2-0dec-11df-bbbe-ede73d0abfb9>
2010-01-31 11:17:55 +0000

    better code
    git-svn-id: https://jdchrome.googlecode.com/svn/trunk@5 2fa9e8c2-0dec-11df-bbbe-ede73d0abfb9

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ var destination, destinationPath, autostart, oneByOne, accelKey, accelAlt, accel
 
 function getOptions(response) {
 	if(response.command == "options") {
-		this.destination = response.destination;
-		this.destinationPath = response.destinationPath;
-		this.autostart = response.autostart;
-		this.oneByOne = response.oneByOne;
-		this.accelKey = response.accelKey;
-		this.accelAlt = response.accelAlt;
-		this.accelCtrl = response.accelCtrl;
-		this.accelShift = response.accelShift;
+			this.destination = response.destination;
+			this.destinationPath = response.destinationPath;
+			this.autostart = response.autostart;
+			this.oneByOne = response.oneByOne;
+			this.accelKey = response.accelKey;
+			this.accelAlt = response.accelAlt;
+			this.accelCtrl = response.accelCtrl;
+			this.accelShift = response.accelShift;
 	}
 }
 
@@ -27,61 +27,64 @@ function onAccelerator(event) {
 		var selection = window.getSelection();
 		var text = selection ? selection.toString() : "";
 
-		if(text.length != 0) {
-			console.log("got:\n" + text);
+		if(text.length == 0) {
+			return;
+		}
 
-			/* extract urls */
-			var urls = text.match(/https?:\/\/.+\..+\s*/gi);
-			console.log("found " + urls.length + " urls:");
+		console.log("got:\n" + text);
 
-			/* trim strings (match() already trims the beginning!) */
+		/* extract urls */
+		var urls = text.match(/https?:\/\/.+\..+\s*/gi);
+		if(urls == null) {
+			console.log("no urls found");
+			return;
+		}
+		else {
+			console.log("found " + urls.length + " url(s):");
+		}
+
+		/* trim strings (match() already trims the beginning!) */
+		for(var r = 0; r < urls.length; r++) {
+			urls[r] = urls[r].replace(/\s*$/, "");
+			console.log(urls[r]);
+		}
+
+		/* set destination argument */
+		var pdestinationpath = "";
+		if(destination == "destination.specify") {
+			pdestinationpath = "&dir=" + destinationPath;
+		}
+
+		/* set autostart argument */
+		var pautostart = "";
+		if(autostart) {
+			pautostart = "&autostart=1";
+		}
+
+		var request; /* easy on garbage collecting? */
+		var baseurl = "http://localhost:9666/flashgot?urls=";
+		if(oneByOne) {
+			/* send a request for each url */
 			for(var r = 0; r < urls.length; r++) {
-				urls[r] = urls[r].replace(/\s*$/, "");
-				console.log(urls[r]);
-			}
-
-			/* set destination argument */
-			var pdestinationpath = "";
-			if(destination == "destination.specify") {
-				pdestinationpath = "&dir=" + destinationPath;
-			}
-
-			/* set autostart argument */
-			var pautostart = "";
-			if(autostart) {
-				pautostart = "&autostart=1";
-			}
-
-			var request; /* easy on garbage collecting? */
-			if(oneByOne) {
-				/* send a request for each url */
-				for(var r = 0; r < urls.length; r++) {
-					request = "http://localhost:9666/flashgot?urls="
-							+ encodeURI(urls[r])
-							+ pdestinationpath
-							+ pautostart;
-
-					var xmlHttp = new XMLHttpRequest();
-					xmlHttp.open("GET", request, true);
-					xmlHttp.send(null);
-					console.log("sent [" + request + "]");
-				}
-			}
-			else {
-				/* join in one string */
-				urls = urls.join("\n");
-
-				/* set variables */
-				request = "http://localhost:9666/flashgot?urls="
-						+ encodeURI(urls)
-						+ pdestinationpath
-						+ pautostart;
+				request = baseurl + encodeURI(urls[r]) + pdestinationpath + pautostart;
 
 				var xmlHttp = new XMLHttpRequest();
 				xmlHttp.open("GET", request, true);
 				xmlHttp.send(null);
-				console.log("sent [" + request + "]")
+				console.log("sent [" + request + "]");
 			}
+		}
+		else {
+			/* join in one string */
+			urls = urls.join("\n");
+
+			/* set variables */
+			request = baseurl + encodeURI(urls) + pdestinationpath + pautostart;
+
+			var xmlHttp = new XMLHttpRequest();
+			xmlHttp.open("GET", request, true);
+			xmlHttp.send(null);
+			console.log("sent [" + request + "]")
 		}
 	}
 }

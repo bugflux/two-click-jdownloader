dddd2f6b918bc2c5392119be44be171f2950f215
bugflux <bugflux@2fa9e8c2-0dec-11df-bbbe-ede73d0abfb9>
2010-02-04 14:36:48 +0000

    added support for double click group download
    git-svn-id: https://jdchrome.googlecode.com/svn/trunk@19 2fa9e8c2-0dec-11df-bbbe-ede73d0abfb9

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ chrome.extension.sendRequest({command : "getOptions"}, getOptions);
 
 /* register event listeners */
 document.addEventListener("keydown", onAccelerator, false);
+document.addEventListener("dblclick", onDoubleClick, false);
+
 
 /* callback for getOptions command message,
  * saves the options in current variables */
@@ -26,6 +28,52 @@ function getOptions(response) {
 	}
 }
 
+/* callback for key press event */
+function onAccelerator(event) {
+	if(acceleratorMatch(event)) {
+		var selection = window.getSelection();
+		if(selection == null) {
+			return;
+		}
+
+		text = selection.toString();
+		console.log("got:\n" + text);
+
+		sendURLs(extractURLs(text));
+	}
+}
+
+/* callback for double click event */
+function onDoubleClick(event) {
+	var selection = window.getSelection();
+	if(selection == null) {
+		return;
+	}
+
+	var host = extractHost(selection.focusNode.data);
+	if((host == null) || (host.length == 0)) {
+		console.log("couldn't find host");
+		return;
+	}
+
+	console.log("selected host: " + host);
+
+	/* extract the URLs */
+	var text = selection.anchorNode.parentNode.innerText;
+	urls = extractURLs(text);
+
+	/* consider only the selected host */
+	var hostUrls = new Array();
+	for(var r = 0; r < urls.length; r++) {
+		if(urls[r].indexOf(host) != -1) {
+			hostUrls.push(urls[r]);
+		}
+	}
+
+	sendURLs(hostUrls);
+}
+
+
 /* checks if the pressed keys match the user defined */
 function acceleratorMatch(e) {
 	return e.keyCode == this.accelKey
@@ -34,21 +82,9 @@ function acceleratorMatch(e) {
 			&& e.shiftKey == this.accelShift;
 }
 
-/* callback for key press event */
-function onAccelerator(event) {
-	if(acceleratorMatch(event)) {
-		var selection = window.getSelection();
-		if(selection) {
-			text = selection.toString();
-			console.log("got:\n" + text);
-		}
-		else {
-			console.log("no selection found");
-			return;
-		}
-
-		sendURLs(extractURLs(text));
-	}
+/* extract a host from a URL */
+function extractHost(url) {
+	return url.match(/https?:\/\/.+?\..+?\//i); /* shortest match */
 }
 
 /* given a block of text return an array with all the found URLs or null, if none found */
